e0d88c257f2515eecb9c858ba38e3aada18b9041
Homely Assist <homeconnecteasy@gmail.com>
2025-02-22 21:02:46 +0530

    new commit

--- src/main/java/org/example/collections/SetCollections.java
+++ src/main/java/org/example/collections/SetCollections.java
@@ -1,10 +1,26 @@
 package org.example.collections;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class SetCollections {
 
     public static void main(String[] args) {
+
+        // to remove duplicate data
+        // or I want to perform look up with value (fast) O(log N)  read/black & AVL
+
+        // java steams
+        //.distinct() ==> Set
+        Set<String> set = new HashSet<>();
+        set.add("Mohit");
+        set.add("Mohit");
+        set.add("Karan");
+
+        // set unique data
+        for(String value: set) {
+            System.out.println(value);
+        }
 
     }
 }
